f5d09e59a57e2ee88066e0ef77ef2dfcda26ad30
Yves P <havokinspiration@gmail.com>
2015-10-18 12:04:57 +0200

    Clean up the abstract Mode class and add comments

--- src/Mode/Mode.php
+++ src/Mode/Mode.php
@@ -15,24 +15,37 @@ use Cake\Core\InstanceConfigTrait;
 use Cake\Network\Request;
 use Cake\Network\Response;
 
+/**
+ * Base class that Maintenance mode should extend
+ */
 abstract class Mode
 {
 
     use InstanceConfigTrait;
 
     /**
-     * Default config
+     * Constructor.
+     * Will set the config using the methods from the InstanceConfigTrait
      *
-     * @var array
+     * @param array $config Array of parameters for the Mode
      */
-    protected $_defaultConfig = [
-        'code' => 503
-    ];
-
     public function __construct($config = [])
     {
         $this->config($config);
     }
 
+    /**
+     * Main method that will be called if the MaintenanceModeFilter has to be used
+     * This method should return the response that will be sent in order to warn the
+     * user that the current request can not be processed because the app is undergoing
+     * maintenance
+     *
+     * Maintenance modes should extend and implement this method to return the proper
+     * response to the user.
+     *
+     * @param \Cake\Network\Request $request Current request being intercepted
+     * @param \Cake\Network\Response $response Current response being sent
+     * @return \Cake\Network\Response|null The response that will be sent
+     */
     abstract public function process(Request $request, Response $response);
 }
